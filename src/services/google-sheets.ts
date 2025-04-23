69c7d6cebd2c15d25e36488c7a0c8128d9114c7d
Игорь Савин <Shakilala84@gmail.com>
2025-04-23 09:10:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/services/google-sheets.ts:4:1
    > Module not found: Can't resolve '@/config/service-account-key.json'
    >   2 | import { google } from 'googleapis';
    >   3 | import { JWT } from 'google-auth-library';
    > > 4 | import serviceAccountKey from '@/config/service-account-key.json';
    >     | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    >   5 | interface serviceAccount {
    >   6 |   private_key: string;
    >   7 |   client_email: string;
    >
    > Import map: aliased to relative './src/config/service-account-key.json' inside of [project]/
    >
    >
    > https://nextjs.org/docs/messages/module-not-found
    >
    >

--- src/services/google-sheets.ts
+++ src/services/google-sheets.ts
@@ -1,7 +1,7 @@
 'use server';
 import { google } from 'googleapis';
 import { JWT } from 'google-auth-library';
-import serviceAccountKey from '../../config/service-account-key.json';
+import serviceAccountKey from '../config/service-account-key.json';
 interface serviceAccount {
   private_key: string;
   client_email: string;
@@ -171,4 +171,5 @@ export async function createSheet(username: string): Promise<void> {
     throw error;
   }
 }
+
 
