092e133b88176668b19e224a67bb7b4342a57d5f
Игорь Савин <Shakilala84@gmail.com>
2025-04-23 19:03:28 +0000

    Remove service-account-key.json file

--- src/services/google-sheets.ts
+++ src/services/google-sheets.ts
@@ -1,7 +1,7 @@
-'use server';
+''use server';
 import { google } from 'googleapis';
 import { JWT } from 'google-auth-library';
-import serviceAccountKey from '../services/service-account-key.json';
+import serviceAccountKey from '../../config/service-account-key.json';
 interface serviceAccount {
   private_key: string;
   client_email: string;
@@ -43,8 +43,6 @@ async function getNextAvailableRow(sheets: any, spreadsheetId: string, sheetName
   const result = await sheets.spreadsheets.values.get({
     spreadsheetId,
     range: `${sheetName}!A:A`,
-    valueRenderOption: 'UNFORMATTED_VALUE',
-    dateTimeRenderOption: 'SERIAL_NUMBER',
   });
 
   const values = result.data.values;
@@ -150,7 +148,7 @@ export async function createSheet(username: string): Promise<void> {
 
       // Write headers to the new sheet
       const headerValues = [
-        ["Дата", "Одометр", "Время отметки медика", "Время карты вставил-вытащил", "Гос номер автобуса", , "Дата", "Одометр", "Литры"],
+        ["Дата", "Одометр", "Время отметки медика", "Время карты вставил-вытащил", "Гос номер автобуса", "", "Дата", "Одометр", "Литры"],
       ];
         
       const headerResource = {
@@ -173,5 +171,3 @@ export async function createSheet(username: string): Promise<void> {
     throw error;
   }
 }
-
-
